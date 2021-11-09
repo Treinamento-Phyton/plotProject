8109213d5e57f5af0be95f901443bdd774c6671b
marcello-mcorreia <93871313+marcello-mcorreia@users.noreply.github.com>
2021-11-08 21:10:08 -0300

    x
    x

--- plot/main.py
+++ plot/main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
 
+fig, ax = plt.subplots()
+ax.plot(t, s)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+fig.savefig("test.png")
+plt.show()
