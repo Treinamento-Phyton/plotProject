dd5eee9c99523d57d0a3810e8a46330219894645
marcello-mcorreia <93871313+marcello-mcorreia@users.noreply.github.com>
2021-11-08 22:17:15 -0300

    melhora plot

--- plot/main.py
+++ plot/main.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+Valor_medio = 1
+frequencia = 2
+tempo_inicial = 0.0
+tempo_final = 2.0
+delta_t = 0.01
+
 # Data for plotting
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2 * np.pi * t)
+t = np.arange(tempo_inicial, tempo_final, delta_t)
+s = Valor_medio + np.sin(frequencia * np.pi * t)
+
+#s = t*t - 3*t + 1
 
 fig, ax = plt.subplots()
 ax.plot(t, s)
